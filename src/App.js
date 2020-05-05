c257187953fe848a5923489e9cc1d7840915eba8
Lawrence Kamau <lawrencekamau5@gmail.com>
2020-05-05 14:46:22 +0000

    Initialize App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import BottomNav from './app/containers/BottomNav';
+
+function App() {
+  return (
+    <div>
+      <BottomNav />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/app/containers/BottomNav/index.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import './style.scss';
+import MenuBar from './MenuBar';
+
+function BottomNav() {
+  const [active, setActive] = useState('events')
+  return (
+    <div className="App" style={{
+      backgroundColor: active === 'events' ? '#5B37B7'
+        : active === 'search' ? '#BE3099'
+        : active === 'featured' ? '#E39A28'
+        : '#1194AA',
+      transition: 'all 50ms linear'
+    }}>
+      <MenuBar
+        active={active}
+        setActive={setActive}
+      />
+    </div>
+  );
+}
+
+export default BottomNav;
